519d4a9fad827792597d5b26de6b533a3cb8d560
Nicholas Koza <NicholasKoza@gmail.com>
2017-10-22 15:04:54 -0700

    Adding license headers

--- src/SwiftCarve/SwiftCarve.ino
+++ src/SwiftCarve/SwiftCarve.ino
@@ -1,5 +1,22 @@
-// Written by Nicholas Koza
-// All rights reserved. Not for redistribution or use without permission.
+/*
+Copyright(C) 2017 Nicholas Koza
+
+This file is part of SwiftCarve.
+
+This program is free software : you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "Config.h"
 #include "Relay.h"
 #include "LED.h"
